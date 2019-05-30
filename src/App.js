0b0509d6a2a96530c25855f22b7ced32798a299c
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-05-30 13:03:58 +0200

    Test of Components with ReactContext is complete

--- src/App.js
+++ src/App.js
@@ -6,7 +6,21 @@ import {
     Rol,
     View,
     SetProperty,
-    PSView} from "perspectives-react";
+    PSView,
+    RolBinding,
+    ContextOfRole,
+    BoundContext,
+    ExternalRole,
+    ExternalViewOfBoundContext,
+    ViewOnExternalRole} from "perspectives-react";
+
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Start the core. 4
 main();
@@ -16,21 +30,73 @@ class App extends Component
   render ()
   {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">perspectives-react-integrated-client</h1>
-        </header>
-        <Context contexttype="model:Perspectives$PerspectivesSysteem" contextinstance="model:User$MijnSysteem">
-          <Rol rol="gebruiker">
-            <View viewname="VolledigeNaam">
-              <GebruikerNaam />
-              <SetProperty propertyname="voornaam">
-                <GebruikerVoornaamInput/>
-              </SetProperty>
-            </View>
-          </Rol>
-        </Context>
-      </div>
+      <Container>
+        <Row>
+          <header className="App-header">
+            <h1>perspectives-react-integrated-client</h1>
+          </header>
+        </Row>
+        <Row>
+          <Form>
+            <Context contexttype="model:Perspectives$PerspectivesSysteem" contextinstance="model:User$MijnSysteem">
+              <Rol rol="gebruiker">
+                <ContextOfRole>
+                  <p>Inside ContextOfRole</p>
+                  <Rol rol="gebruiker">
+                    <View viewname="VolledigeNaam">
+                      <Card>
+                        <Card.Body>
+                          <GebruikerNaam />
+                        </Card.Body>
+                      </Card>
+                    </View>
+                  </Rol>
+                </ContextOfRole>
+                <View viewname="VolledigeNaam">
+                  <Card>
+                    <Card.Body>
+                      <GebruikerNaam />
+                    </Card.Body>
+                  </Card>
+                  <SetProperty propertyname="voornaam">
+                    <GebruikerVoornaamInput/>
+                  </SetProperty>
+                </View>
+              </Rol>
+              <Rol rol="trustedCluster">
+                <RolBinding>
+                  <p>Inside RolBinding</p>
+                  <View viewname="Kaartje">
+                    <TrustedCluster_BuitenRol_Kaartje/>
+                  </View>
+                </RolBinding>
+                <BoundContext>
+                  <p>Inside BoundContext, arriving at TrustedCluster.</p>
+                  <Rol rol="clusterGenoot">
+                    <View viewname="Adressering">
+                      <ClusterGenoot/>
+                    </View>
+                  </Rol>
+                  <ExternalRole>
+                    <p>Inside ExternalRole, arriving at the buitenRol of TrustedCluster</p>
+                    <View viewname="Kaartje">
+                      <TrustedCluster_BuitenRol_Kaartje/>
+                    </View>
+                  </ExternalRole>
+                  <ViewOnExternalRole viewname="Kaartje">
+                    <p>Inside ViewOnExternalRole, arriving at the View Kaartje of the buitenRol of TrustedCluster</p>
+                    <TrustedCluster_BuitenRol_Kaartje />
+                  </ViewOnExternalRole>
+                </BoundContext>
+                <ExternalViewOfBoundContext viewname="Kaartje">
+                  <p>Inside ExternalViewOfBoundContext, arriving at the View Kaartje of the buitenRol of TrustedCluster</p>
+                  <TrustedCluster_BuitenRol_Kaartje />
+                </ExternalViewOfBoundContext>
+              </Rol>
+            </Context>
+          </Form>
+        </Row>
+      </Container>
     );
   }
 }
@@ -51,5 +117,24 @@ function GebruikerVoornaamInput (props)
     <input defaultValue={props.defaultvalue} onBlur={e => props.setvalue(e.target.value)} />
     </fieldset>);
 }
+
+function TrustedCluster_BuitenRol_Kaartje ()
+{
+  return (
+    <PSView.Consumer>
+      {value => <p><label>TrustedCluster:</label>{value.naam}</p>}
+    </PSView.Consumer>
+  );
+}
+
+function ClusterGenoot ()
+{
+  return (
+    <PSView.Consumer>
+      {value => <p>Clustergenoot {value.voornaam} heeft url: {value.url}</p>}
+    </PSView.Consumer>
+  );
+}
+
 
 export default App;
