41154d59052b421d83415609e69eacaf1959994f
Joop Ringelberg <joop.ringelberg@gmail.com>
2019-06-14 13:59:36 +0200

    perspectivesGlobals no longer included in index.html

--- src/externals.js
+++ src/externals.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import * as PerspectivesReact from "perspectives-react";
 
-import PerspectivesGlobals from "./perspectivesGlobals.js"
+import PerspectivesGlobals from "./perspectivesGlobals.js";
 
 window.React = React;
 window.PerspectivesReact = PerspectivesReact;
