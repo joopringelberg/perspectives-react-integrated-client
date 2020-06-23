703511741bcfa92af0bcde2c32f172b0ba209800
Joop Ringelberg <joop.ringelberg@gmail.com>
2020-06-23 16:14:31 +0200

    Removed chunkFileName entry from webpack config as we do no longer load dynamically through webpack

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,9 @@ const path = require("path");
 module.exports = {
   entry:
     { index: path.join(__dirname, "src/index.js" )
-    // , Perspectives: path.join(__dirname, "src/Perspectives.js" )
   },
-  // output: {path: path.join(__dirname, 'public'), filename: "bundle.js"},
   output: {
       filename: '[name].js',
-      chunkFilename: '[name].js',
       path: path.resolve(__dirname, 'public')
     },
   externals: {},
