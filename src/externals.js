85c589a52305d0cf363ba1ed9fc5aad5f8f147ef
Joop Ringelberg <joop.ringelberg@gmail.com>
2020-06-25 12:34:08 +0200

    We now also put react-dom on the global (window) scope so we can externalise it in libraries such as the screens.

--- src/externals.js
+++ src/externals.js
@@ -1,9 +1,17 @@
+/*
+This module exports a number of modules to the global scope (window object).
+The Webpack configuration of the Screens modules externalises these modules.
+The same holds for perspectives-react.
+*/
 import React from "react";
 
 import * as PerspectivesReact from "perspectives-react";
 
 import PerspectivesGlobals from "./perspectivesGlobals.js";
 
+import * as ReactDOM from "react-dom"
+
 window.React = React;
 window.PerspectivesReact = PerspectivesReact;
 window.PerspectivesGlobals = PerspectivesGlobals;
+window.ReactDOM = ReactDOM;
