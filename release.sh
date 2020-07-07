5754fd50ecf8c77f90c4ef181d85a2b8e9aa66a3
Joop Ringelberg <joop.ringelberg@gmail.com>
2020-07-07 15:49:39 +0200

    delete release script: it contains a github token!

--- release.sh
+++
@@ -1,7 +0,0 @@
-export GH_TOKEN=b597aaeba76d28365863f87b7c7aff11c9b06208
-electron-builder build -p always
-
-## Overriding configuration for local testing:
-#export AWS_ACCESS_KEY_ID=minioadmin
-#export AWS_SECRET_ACCESS_KEY=minioadmin
-#electron-builder build -p always --config.publish.provider=s3 --config.publish.endpoint=http://127.0.0.1:9000 --config.publish.bucket=test-update -c.compression=store
